a21f65a39c615fa257ac4ae86d9cb7a23d374227
danturne <danturne@yahoo.com>
2015-07-07 09:33:08 -1000

    initial commit brute force, second test timing out

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -5,7 +5,43 @@
  *  @returns the smallest positive number that is evenly divisible by all numbers
  */
 module.exports = function(numbers){
-  // do work here
 
-  return 0;
+  function isDivisibleThroughRange(numberToBeTested, maximumRangeNumber) {
+
+    for( var i = 2; i <= maximumRangeNumber; i++) {
+
+      if(numberToBeTested%i !== 0) {
+
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  if(typeof numbers !== 'number') {
+
+    throw new TypeError("Function requires a number");
+  }
+
+  if(isNaN(numbers)) {
+
+    throw new TypeError("Function cannot take NaN");
+  }
+
+  numbers = Math.abs(numbers);
+
+  var candidateNumber = numbers;
+
+  if(candidateNumber%2 !== 0) {
+
+    candidateNumber++;
+  }
+
+  while(!isDivisibleThroughRange(candidateNumber, numbers)) {
+
+    candidateNumber += 2;
+  }
+
+  return candidateNumber;
 };
